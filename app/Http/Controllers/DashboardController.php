cb893bd888f59af5b849e052ae8fe7653810bc24
Muh Rizki Pratama <muhrizkipratama12@gmail.com>
2025-07-17 16:12:25 +0800

    Halaman: Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ItemPengeluaranBarang;
+use App\Models\PengeluaranBarang;
+use App\Models\Product;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        // Query Jumlah Pendapatan per bulan per Tahun
+        $bulanIni = Carbon::now()->month;
+        $tahunIni = Carbon::now()->year;
+
+        $totalOrder = PengeluaranBarang::whereMonth('created_at', $bulanIni)->whereYear('created_at', $tahunIni)->count();
+
+        $totalPendapatan = PengeluaranBarang::whereMonth('created_at', $bulanIni)->whereYear('created_at', $tahunIni)->sum('total_harga');
+        $totalPendapatan = "Rp. " . number_format($totalPendapatan);
+
+        // Query Hitung User
+        $totalUsers = User::count();
+        // Query Hitung Produk
+        $totalProduk = Product::count();
+
+        // Query 5 Transaksi Terbaru
+        $latestOrder = PengeluaranBarang::latest()->take(5)->get()->map(function($item){$item->tanggal_transaksi = Carbon::parse($item->created_at)->locale('id')->translatedFormat('l,d-m-Y');
+            return $item;
+        });
+
+        // Query Barang Terlaris bulan berjalan
+        $produkTerlaris = ItemPengeluaranBarang::select('nama_produk')->selectRaw('SUM(qty) as total_terjual')->whereMonth('created_at', $bulanIni)->whereYear('created_at', $tahunIni)->groupBy('nama_produk')->orderByDesc('total_terjual')->limit(5)->get();
+
+        return view('dashboard.index', compact('totalUsers', 'totalProduk', 'totalPendapatan', 'totalOrder', 'latestOrder', 'produkTerlaris'));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,10 +3,75 @@
 @section('content_title', 'Dashboard')
 
 @section('content')
-<div class="card">
-    <x-alert :errors='$errors' type='warning'/>
-    <div class="card-body">
-        Welcome to POS Aplication, <strong>{{ auth()->user()->name }}</strong>
+<x-alert :errors='$errors' type='warning'/>
+<div class="row">
+    <x-dashboard-card type="bg-info" icon="fas fa-users" label="Total Users" value="{{ $totalUsers }}"/>
+    <x-dashboard-card type="bg-warning" icon="fas fa-suitcase" label="Total Produk" value="{{ $totalProduk }}"/>
+    <x-dashboard-card type="bg-success" icon="fas fa-shopping-bag" label="Total Order" value="{{ $totalOrder }}"/>
+    <x-dashboard-card type="bg-teal" icon="fas fa-dollar-sign" label="Total Pendapatan" value="{{ $totalPendapatan }}"/>
+</div>
+<div class="row">
+    <div class="col-8">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Transaksi Terakhir</h4>
+            </div>
+            <div class="card-body">
+                <table class="table table-sm">
+                    <thead>
+                        <tr>
+                            <th>Tanggal Transaksi</th>
+                            <th>Nomor Transaksi</th>
+                            <th>Total Harga</th>
+                            <th>Jumlah Item</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($latestOrder as $o )
+                        <tr>
+                            <td>{{ $o->tanggal_transaksi }}</td>
+                            <td>{{ $o->nomor_pengeluaran }}</td>
+                            <td>{{ $o->items->count() }} <small>Item</small></td>
+                            <td>Rp. {{ number_format( $o->total_harga) }}</td>
+                        </tr>                            
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-footer">
+                Menampilkan 5 Data Transaksi Terakhir
+            </div>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Produk Terlaris</h4>
+            </div>
+            <div class="card-body">
+                <table class="table table-sm">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Produk</th>
+                            <th>Jumlah Terjual</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ( $produkTerlaris as $i=>$p )
+                            <tr>
+                                <td>{{ $i + 1 }}</td>
+                                <td>{{ $p->nama_produk }}</td>
+                                <td>{{ $p->total_terjual }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-footer">
+                Menampilkan 5 Produk Terlaris
+            </div>
+        </div>
     </div>
 </div>
 @endsection
